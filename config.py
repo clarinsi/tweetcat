100766ec1f8e14918d3e2de76a63a527f62f7dbe
Nikola Ljubešić <nljubesi@gmail.com>
2017-04-13 10:15:52 +0000

    V0.1 data deleted

--- config.py
+++
@@ -1,9 +0,0 @@
-LANG_ID_THRESHOLD=0.01 # threshold of percentage of tokens in the timeline from the function word list
-
-SLEEP_BETWEEN_ITERATIONS=60*60*12 # number of seconds
-ITERATION_COUNT=0 # 0 for unlimited number of iterations
-
-CONSUMER_KEY=''
-CONSUMER_SECRET=''
-ACCESS_TOKEN=''
-ACCESS_TOKEN_SECRET=''
