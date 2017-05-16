dbd09dcd6af59c9a72b13753c9db94f2c67a8748
Nikola Ljubešić <nljubesi@gmail.com>
2017-05-16 07:24:09 +0000

    US and BI data swapped

--- 1-harvest/bi_geo.py
+++ 1-harvest/bi_geo.py
@@ -6,22 +6,22 @@ ACCESS_TOKEN_SECRET=''
 
 BATCH_SIZE=1000 # number of tweets written to disk together in a file
 
-#MODE='GEO'
-MODE='LANG'
+MODE='GEO'
+#MODE='LANG'
 
 # define if MODE is GEO, ignore if MODE is LANG
 # lower left corner, can be obtained from http://www.latlong.net
-MINLAT=22
-MINLON=-125
+MINLAT=50
+MINLON=-11
 # upper right corner
-MAXLAT=50
-MAXLON=-66
+MAXLAT=60
+MAXLON=2
 # language codes of interest, list available from https://github.com/saffsd/langid.py
 # will not be used during the collection process, but at its end if filter_geo.py is run
-LANGID_GEO=['en','es']
+LANGID_GEO=[]
 # country codes of interest, https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
 # will not be used during the collection process, but at its end if filter_geo.py is run
-COUNTRIES=['US']
+COUNTRIES=['GB','IE']
 
 # define if MODE is LANG, ignore if MODE is GEO
 SEEDS='seeds.hbs'
---
+++ 1-harvest/filter_geo.py
@@ -0,0 +1,44 @@
+from langid import classify
+import sys
+import os
+import re
+import json
+import gzip
+
+remove_specific_re=re.compile(r'#\w+|@\w|https?://[\w/_.-]+',re.UNICODE)
+space_re=re.compile(r'\s+',re.UNICODE)
+
+if __name__=='__main__':
+  PROJECT=sys.argv[1]
+  if not os.path.isfile(PROJECT+'.py'):
+    sys.stderr.write('There is no configuration file '+PROJECT+'.py for project '+PROJECT+'\n')
+    sys.exit(1)
+  exec('from '+PROJECT+' import *')
+  users={}
+  num=0
+  for file in os.listdir(PROJECT):
+    for status in json.load(gzip.open(os.path.join(PROJECT,file))):
+      num+=1
+      if num%10000==0:
+        print 'Read',num
+      if len(COUNTRIES)>0:
+        if status['place']==None:
+          continue
+        if status['place']['country_code'] not in COUNTRIES:
+          continue
+      user=status['user']['screen_name']
+      if user not in users:
+        users[user]=[]
+      users[user].append(status)
+  print 'Read all together',num
+  os.makedirs(PROJECT+'.filter')
+  num=0
+  print 'Writing down started'
+  for user in users:
+    if len(LANGID_GEO)==0:
+      continue
+    if classify(' '.join([space_re.sub(' ',remove_specific_re.sub(' ',status['text'])).strip() for status in users[user]]))[0] not in LANGID_GEO:
+      continue
+    num+=len(users[user])
+    gzip.open(os.path.join(PROJECT+'.filter',user+'.gz'),'w').write(json.dumps(users[user]))
+  print 'Wrote all together',num
